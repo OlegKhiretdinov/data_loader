789233b57ca564188737873a1a116e2a1d32567b
OlegKhiretdinov <oleg@oleg.win>
2024-07-29 21:12:54 +0300

    feat: добавлен вывод файлов

--- file_storage/app.py
+++ file_storage/app.py
@@ -1,7 +1,21 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request, render_template
+import os
+from urllib.parse import urljoin
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'data/'
+
+
+@app.route('/')
+def home():
+    upload_folder_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        app.config['UPLOAD_FOLDER']
+    )
+    files = os.listdir(upload_folder_path)
+    base_url = request.base_url
+    files_url = map(lambda file: f'{base_url}download/{file}', files)
+    return render_template('/pages/home_page.html', files=files_url)
 
 
 @app.route('/download/<string:filename>')
