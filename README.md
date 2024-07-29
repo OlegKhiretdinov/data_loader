bdffbaaa63e29a720b45eb93fa7c04648a9b3f71
OlegKhiretdinov <oleg@oleg.win>
2024-07-29 16:23:02 +0300

    feat: добавлен запуск в docker

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-http://127.0.0.1:5001/download/test.csv
+http://127.0.0.1:5001/download/test.csv  
+docker run --rm -p 5000:5000 -p 5001:5001 web_app
+
+При запуске через докер POSTGRES_HOST=postgres
--- db/database.py
+++ db/database.py
@@ -4,6 +4,16 @@ from dotenv import load_dotenv
 from psycopg2 import pool
 
 load_dotenv()
-DATABASE_URL = os.getenv('DATABASE_URL')
+# DATABASE_URL = os.getenv('DATABASE_URL')
+
+POSTGRES_DB = os.getenv('POSTGRES_DB')
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+POSTGRES_PORT = os.getenv('POSTGRES_PORT')
+POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+
+print("POSTGRES_ENV", POSTGRES_HOST)
+
+DATABASE_URL = f"user={POSTGRES_USER} password={POSTGRES_PASSWORD} host={POSTGRES_HOST} port={POSTGRES_PORT} dbname={POSTGRES_DB}"
 
 connections = pool.SimpleConnectionPool(1, 100, DATABASE_URL)
